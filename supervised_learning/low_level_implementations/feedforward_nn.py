171a83da7f191a04c9ed0db4daa4d5785a8b097c
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2023-06-13 19:09:39 +0100

    Layer __repr__; Dense grad weights; Series class graph connection methods. NN WIP

--- supervised_learning/low_level_implementations/feedforward_nn.py
+++ supervised_learning/low_level_implementations/feedforward_nn.py
@@ -7,10 +7,10 @@ Maths to code
 =============
 
 X --> input_X
-Z --> output_Z
+Z --> preactivation_Z
 A --> activation_A
-Yhat -->
-Y -->
+Yhat --> prediction_Yhat
+Y --> label_Y
 W --> weights_W
 B --> biases_B
 """
@@ -30,19 +30,23 @@ class Layer:
         self.right_neighbour = None
 
         # Forward prop edges in and out
-        self.layer_input = None
-        self.layer_output = None
+        self.layer_input = np.array(None)
+        self.layer_output = np.array(None)
 
         # Back prop edges in and out
-        self.grad_input_from_right = None
-        self.grad_output_to_left = None
+        self.grad_input_from_right = np.array(None)
+        self.grad_output_to_left = np.array(None)
 
     def __repr__(self):
         """
         When print called on the object, return 'Layer: <class layer name>'.
         This can be added to in subclass (specific layer) representations
         """
-        return f"Layer: {self.__class__.__name__}. "
+        return f"""
+        ========================================================
+        Layer: {self.__class__.__name__}
+        Input shape: {self.layer_input.shape}. Output shape: {self.layer_output.shape}
+        """
 
     def forward_pass(self):
         pass
@@ -63,8 +67,7 @@ class Input(Layer):
         super().__init__()
 
         # Forward prop edges in and out
-        self.layer_input = layer_input
-        self.layer_output = None
+        self.layer_output = layer_input
 
 
 class Dense(Layer):
@@ -83,11 +86,11 @@ class Dense(Layer):
 
         # Feedforward connections in and out
         self.layer_input = left_neighbour.layer_output
-        self.layer_output = None
+        # self.layer_output = None
 
         # Gradient from right neighbour
         self.grad_input_from_right = right_neighbour.grad_output_to_left
-        self.grad_output_to_left = None
+        # self.grad_output_to_left = None
 
         # Parameters passed in on instantiation
         self.n_neurons = n_neurons
@@ -99,9 +102,16 @@ class Dense(Layer):
         self.initialise_weights()
         self.initialise_bias()
 
+        # Initialise grad of weights and bias vector. Same shape as weights and bias, so just copy initialised
+        self.grad_weights = self.weights.copy()
+        self.grad_bias = self.bias.copy()
+
     def __repr__(self):
-        # TODO
-        pass
+        superclass_repr = super().__repr__()
+        dense_stub = f"""
+        Weights shape: ({self.weights.shape}). Bias shape: ({self.bias.shape})
+        """
+        return superclass_repr + dense_stub
 
     def initialise_weights(self):
         """
@@ -117,8 +127,19 @@ class Dense(Layer):
         self.layer_output = np.dot(self.weights, self.layer_input.shape[0]) + self.bias
 
     def backward_pass(self):
+        """
+        This function takes as input dC/dZ_l
+
+        Returns:
+
+        """
         # TODO
-        pass
+        # dC/dA_l-1
+        self.grad_output_to_left = None  # TODO
+        # dC/dW_l
+        self.grad_weights = None    # TODO
+        # dC/db_l
+        self.grad_bias = None  # TODO
 
 
 class Relu(Layer):
@@ -128,6 +149,15 @@ class Relu(Layer):
 
     def __init__(self):
         super().__init__()
+
+    def forward_pass(self):
+        # TODO
+        pass
+
+    def backward_pass(self):
+        # TODO
+        pass
+
 
 # Softmax
 class Softmax(Layer):
@@ -141,15 +171,45 @@ class Softmax(Layer):
 
 class Series:
 
-    def __init__(self):
-        self.layers = []
+    def __init__(self, layers=None):
+        """
 
+        Args:
+            layers (List or None): optional argument to specify network in list form.
+            e.g. [Dense(2), Relu(), Dense(2), Softmax()]
+        """
+        if layers is None:
+            layers = []
+        self.layers = layers
 
     def add(self, layer):
         self.layers.append(layer)
 
-
-    def forward_pass(self, inputs):
+    def forward_pass(self):
         activations = None    # TODO
         return activations
+
+    def connect_forward(self):
+        for idx, layer in enumerate(self.layers):
+            if idx == 0:
+                continue
+            layer.left_neighbour = self.layers[idx - 1]
+            layer.layer_input = self.layers[idx - 1].layer_output
+
+    def connect_backward(self):
+        reversed_list = self.layers[::-1]    # [::-1] reverses list to start with final layer
+        for idx, layer in enumerate(reversed_list):
+            if idx == 0:
+                continue
+            layer.right_neighbour = reversed_list[idx - 1]
+            layer.grad_input_from_right = reversed_list[idx - 1].grad_output_to_left
+
+    def __call__(self, input_array):
+
+        # Turn np.array `input_array` into layer object Input, and append to front of layer list
+        self.layers = [Input(input_array)] + self.layers
+
+        # Connect up network
+        self.connect_forward()
+        self.connect_backward()
 
