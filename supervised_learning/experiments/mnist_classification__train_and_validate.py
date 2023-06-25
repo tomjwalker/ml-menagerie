4ca9bd9c5dd36283c3b088f0cf83680e52166b01
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2023-06-25 12:31:12 +0100

    Model save/load functionality implemented

--- supervised_learning/experiments/mnist_classification__train_and_validate.py
+++ supervised_learning/experiments/mnist_classification__train_and_validate.py
@@ -9,7 +9,8 @@ from supervised_learning.low_level_implementations.feedforward_nn.costs_and_metr
 from supervised_learning.low_level_implementations.feedforward_nn.layers import Dense, Relu, Softmax, BatchNorm
 from supervised_learning.low_level_implementations.feedforward_nn.models import SeriesModel
 from supervised_learning.low_level_implementations.feedforward_nn.optimisers import GradientDescentOptimiser
-from supervised_learning.low_level_implementations.feedforward_nn.tasks import (TrainingTask, EvaluationTask, Loop)
+from supervised_learning.low_level_implementations.feedforward_nn.tasks import (TrainingTask, EvaluationTask,
+                                                                                ModelSaveTask, Loop)
 
 import matplotlib.pyplot as plt
 
@@ -84,14 +85,14 @@ def plot_metric_logs_vs_gradient_norms(metric_log_training, metric_log_evaluatio
 
     # Display text for the maximum of the metric_log_training
     axs[0].text(
-        max_metric_log_training, metric_log_training[metric_name][max_metric_log_training],    # x, y
+        max_metric_log_training, metric_log_training[metric_name][max_metric_log_training],  # x, y
         f"{metric_log_training[metric_name][max_metric_log_training]:.2f}",
         horizontalalignment="right", verticalalignment="bottom"
     )
 
     # Display also the equivalent value from the metric_log_evaluation at the same x-coordinate
     axs[0].text(
-        max_metric_log_training, metric_log_evaluation[metric_name][max_metric_log_training],    # x, y
+        max_metric_log_training, metric_log_evaluation[metric_name][max_metric_log_training],  # x, y
         f"{metric_log_evaluation[metric_name][max_metric_log_training]:.2f}",
         horizontalalignment="right", verticalalignment="top"
     )
@@ -129,18 +130,25 @@ def save_metric_logs(metric_log_training, metric_log_evaluation, metric_name, me
 # saving the outputs of the training loop
 DATA_CACHE_DIR = "./data_cache"
 PLOTS_DIR = "./plots"
-MODEL_WEIGHTS_DIR = "./model_weights"
+MODEL_CHECKPOINTS_DIR = "./model_checkpoints"
+
+# RUN_SETTINGS = {
+#     "model_name": "mnist_ffnn_dense_50_batchnorm",
+#     "num_epochs": 10,  # 60
+#     "train_abs_samples": None,  # TODO: remove this after debugging (set to None) - then full training set is used
+#     "clip_grads_norm": True,
+# }
 
 RUN_SETTINGS = {
-    "model_name": "mnist_ffnn_dense_50_batchnorm_clipnorm",
-    "num_epochs": 10,    # 60
-    "train_abs_samples": None,    # TODO: remove this after debugging (set to None) - then full training set is used
+    "model_name": "mnist_ffnn_dense_50_batchnorm",
+    "num_epochs": 2,  # 60
+    "train_abs_samples": 200,  # TODO: remove this after debugging (set to None) - then full training set is used
     "clip_grads_norm": True,
 }
 
 # Filepath prefix specifies the run settings as defined above
 run_suffix = "__".join([f"{key}_{str(value)}" for key, value in RUN_SETTINGS.items()])
-run_suffix = run_suffix.replace(".", "_")    # If any values are floats, replace "." with "_" for filename
+run_suffix = run_suffix.replace(".", "_")  # If any values are floats, replace "." with "_" for filename
 
 # Load MNIST dataset
 features, labels = load_mnist()
@@ -156,13 +164,13 @@ features, labels = preprocess_mnist(features, labels)
 #         Softmax(),
 #     ]
 architecture = [
-        Dense(50),
-        BatchNorm(),
-        Relu(),
-        Dense(10),
-        BatchNorm(),
-        Softmax(),
-    ]
+    Dense(50),
+    BatchNorm(),
+    Relu(),
+    Dense(10),
+    BatchNorm(),
+    Softmax(),
+]
 
 # Define training task
 training_task = TrainingTask(
@@ -184,12 +192,24 @@ evaluation_task = EvaluationTask(
     metrics=[CategoricalCrossentropyCost(), AccuracyMetric()],
 )
 
+# Instantiate a model saver
+# TODO: replace attribute store of metrics within train/eval task with dicts, rather than lists. Then can access with
+#   names rather than indices
+model_saver = ModelSaveTask(
+    monitoring_task=evaluation_task,
+    metric_type=CategoricalCrossentropyCost(),
+    save_every_n_iters=10,
+    save_dir=MODEL_CHECKPOINTS_DIR,
+    save_filename=run_suffix,
+)
+
 # Run training loop
 loop = Loop(
     dataset=(features, labels),
     model=model,
     training_task=training_task,
     evaluation_task=evaluation_task,
+    model_save_task=model_saver,
 )
 
 loop.run(
@@ -202,7 +222,6 @@ loop.run(
 
 # Plot training and evaluation metric logs
 for metric in loop.training_task.metric_log.keys():
-
     save_metric_logs(
         loop.training_task.metric_log,
         loop.evaluation_task.metric_log,
@@ -224,7 +243,3 @@ for metric in loop.training_task.metric_log.keys():
 predictions = loop.model.forward_pass(loop.features_val, mode="infer")
 save_filepath = f"{PLOTS_DIR}/sampled_predictions__{run_suffix}.png"
 show_digit_samples(loop.features_val, loop.labels_val, predictions, m_samples=10, save_filepath=save_filepath)
-
-
-
-
